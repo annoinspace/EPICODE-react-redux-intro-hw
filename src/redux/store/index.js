e288fdeb128d2e71ba7ea8225473691d378bd4f7
annoinspace <aneesahalmask@gmail.com>
2022-12-06 14:47:14 +0000

    encryption working

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,13 +2,35 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import favouritesReducer from "../reducers/favouritesReducer"
 import jobsReducer from "../reducers/jobsReducer"
 
+//setting uo the local storage so data is not lost on page refresh
+import localStorage from "redux-persist/lib/storage"
+import { persistReducer, persistStore } from "redux-persist"
+import { encryptTransform } from "redux-persist-transform-encrypt"
+
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_SECRET_KEY
+    })
+  ]
+}
+
 const bigReducer = combineReducers({
   favourites: favouritesReducer,
   jobs: jobsReducer
 })
 
-const store = configureStore({
-  reducer: bigReducer
+const persistedReducer = persistReducer(persistConfig, bigReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false
+    })
+  }
 })
 
-export default store
+export const persistor = persistStore(store)
